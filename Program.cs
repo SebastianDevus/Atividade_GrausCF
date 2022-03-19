434b7d29f8a09f424748b5ea45e6da44ff421a4c
SebastianDevus <sebastianjogosextra@gmail.com>
2022-03-19 16:35:19 -0300

    Limitada a quantidade de numeros apos a virgula

--- Program.cs
+++ Program.cs
@@ -5,4 +5,4 @@ grausC = Convert.ToDouble(Console.ReadLine()!);
 
 grausF = grausC * 1.8 + 32;
 
-Console.WriteLine($"O equivalente em °F é {grausF}");
+Console.WriteLine($"O equivalente em °F é {grausF:N2}");
