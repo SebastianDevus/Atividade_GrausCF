bf50fc0faec2aa80e0b36ac53631a7a878233e23
SebastianDevus <sebastianjogosextra@gmail.com>
2022-03-19 15:04:52 -0300

    Atividade realizada

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿double grausF, grausC;
+
+Console.Write("Insira a temperatura (°C): ");
+grausC = Convert.ToDouble(Console.ReadLine()!);
+
+grausF = grausC * 1.8 + 32;
+
+Console.WriteLine($"O equivalente em °F é {grausF}");
